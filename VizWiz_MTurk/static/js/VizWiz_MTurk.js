fae1293e4d1fa5b5de9d5a6d5dfb9c90ccb373d8
陈崇彦 <chongyanchen_hci@utexas.edu>
2020-06-10 23:26:56 -0500

    external_image fixed

--- VizWiz_MTurk/static/js/VizWiz_MTurk.js
+++ VizWiz_MTurk/static/js/VizWiz_MTurk.js
@@ -1,3 +1,3 @@
 var searchParams = new URLSearchParams(window.location.search);
 var imageURLvalue = searchParams.get("img1");
-document.getElementById("VizWiz_img").src=imageURLvalue;
+document.getElementById("IMAGE").src=imageURLvalue;
