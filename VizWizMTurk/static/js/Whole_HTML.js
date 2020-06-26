c6d55a05d015fe3f7e54547685d4622c9202493f
陈崇彦 <chongyanchen_hci@utexas.edu>
2020-06-25 22:28:31 -0500

    add task2-select correct object category

--- VizWizMTurk/static/js/Whole_HTML.js
+++ VizWizMTurk/static/js/Whole_HTML.js
@@ -10,10 +10,41 @@ function ReportProblem() {
 function PartialExpertiseChecked() {
     var Expertise = document.getElementById("EK2");
     var Partial = document.getElementById("EK3");
-    var task3 = document.getElementById("task3");
+    var task4 = document.getElementById("task4");
     if (Expertise.checked == true || Partial.checked==true){
-        task3.style.display = "block";
+        task4.style.display = "block";
     } else {
-        task3.style.display = "none";
+        task4.style.display = "none";
     }
   }
+
+function DisableOther_P(){
+    
+    if (document.getElementById("Perception").checked==true){
+        document.getElementById("EK1").disabled=true;
+        document.getElementById("EK2").disabled=true;
+        document.getElementById("EK3").disabled=true;
+        document.getElementById("NotSure").disabled=true;}
+    else{
+        
+        document.getElementById("EK1").disabled=false;
+        document.getElementById("EK2").disabled=false;
+        document.getElementById("EK3").disabled=false;
+        document.getElementById("NotSure").disabled=false;
+    }
+}
+function DisableOther_NS(){
+    if (document.getElementById("NotSure").checked==true){
+        document.getElementById("EK1").disabled=true;
+        document.getElementById("EK2").disabled=true;
+        document.getElementById("EK3").disabled=true;
+        document.getElementById("Perception").disabled=true;}
+    else{
+        
+        document.getElementById("EK1").disabled=false;
+        document.getElementById("EK2").disabled=false;
+        document.getElementById("EK3").disabled=false;
+        document.getElementById("Perception").disabled=false;
+    }
+
+}
