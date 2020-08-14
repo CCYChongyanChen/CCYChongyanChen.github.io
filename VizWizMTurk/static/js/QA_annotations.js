d3fb5fc36591d00677340a83a648ffdd3169321b
陈崇彦 <chongyanchen_hci@utexas.edu>
2020-08-13 23:18:34 -0500

    delete certain polygons

--- VizWizMTurk/static/js/QA_annotations.js
+++ VizWizMTurk/static/js/QA_annotations.js
@@ -1,6 +1,7 @@
 var searchParams = new URLSearchParams(window.location.search);
 var img_id = searchParams.get("img1");
 var dataset=img_id.split("_")
+var answer_element=document.getElementById("Answers")
 function getUnique(array){
     var uniqueArray = [];
     // Loop through array values
@@ -14,20 +15,43 @@ function getUnique(array){
 
 $.ajax({
     type:'get',
-    url:"https://raw.githubusercontent.com/CCYChongyanChen/CCYChongyanChen.github.io/master/VizWizMTurk/static/QA_annotations/"+dataset[1]+".json",
+    url:"https://raw.githubusercontent.com/CCYChongyanChen/CCYChongyanChen.github.io/master/VizWizMTurk/static/QA_annotations/"+dataset[1]+"_clean.json",
     dataType:'json',
     success:function(data){
-        var only_answers=""
-        var question=(data[parseInt(dataset[2])]["question"])
-        var Answers=(data[parseInt(dataset[2])].answers)
-        var temp_answers=[]
-        for (k=0;k<10;k++){
-            temp_answers.push(Answers[k].answer)}
-        var UniqAnswer=getUnique(temp_answers)
-        for (i=0;i<UniqAnswer.length;i++){
-            only_answers+=(i+"."+UniqAnswer[i]+"<br>")}
+
+        for (k=0;k<data.length;k++){
+            if (data[k]["image"]==img_id){
+                var Answers=data[k]["answers"]
+                var question=data[k]["question"]
+                for (j=0;j<Answers.length;j++){
+                    if (j==0){
+                        answer_element.insertAdjacentHTML('beforeend','<button class="btn active" type="button" id=answer'+j.toString()+'>'+Answers[j]+'</button>')
+                    }
+                    else{
+                    answer_element.insertAdjacentHTML('beforeend','<button class="btn" type="button" id=answer'+j.toString()+'>'+Answers[j]+'</button>')
+                }
+                }
+                }
+
+        }
+
+        var answers=document.getElementById("Answers")
+        var btns =answers.getElementsByTagName("button")
+        var count = btns.length
+        for (var i = 0; i < btns.length; i++) {
+            btns[i].addEventListener("click", function() {
+            var current = document.getElementsByClassName("active");
+            var whichbutton=this.id.slice(0,6);
+            if (whichbutton=="answer"){
+                current[0].className = current[0].className.replace(" active", "");
+                this.className += " active";
+            }
+            
+            });
+        }
+
+
         document.getElementById("question").innerHTML=question
-        document.getElementById("answers").innerHTML=only_answers
     }
 })
 
