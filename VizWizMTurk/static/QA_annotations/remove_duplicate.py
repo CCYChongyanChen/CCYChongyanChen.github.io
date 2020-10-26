3a794758449371bedae388214ec4f3bf6d1bcf8d
陈崇彦 <chongyanchen_hci@utexas.edu>
2020-10-25 23:30:47 -0500

    delete subquestions

---
+++ VizWizMTurk/static/QA_annotations/remove_duplicate.py
@@ -0,0 +1,130 @@
+import json
+import os
+import sys
+import re
+import numpy as np
+from spellchecker import SpellChecker
+
+
+contractions = {"aint": "ain't", "arent": "aren't", "cant": "can't", "couldve": "could've", "couldnt": "couldn't", \
+							 "couldn'tve": "couldn’t’ve", "couldnt’ve": "couldn’t’ve", "didnt": "didn’t", "doesnt": "doesn’t", "dont": "don’t", "hadnt": "hadn’t", \
+							 "hadnt’ve": "hadn’t’ve", "hadn'tve": "hadn’t’ve", "hasnt": "hasn’t", "havent": "haven’t", "hed": "he’d", "hed’ve": "he’d’ve", \
+							 "he’dve": "he’d’ve", "hes": "he’s", "howd": "how’d", "howll": "how’ll", "hows": "how’s", "Id’ve": "I’d’ve", "I’dve": "I’d’ve", \
+							 "Im": "I’m", "Ive": "I’ve", "isnt": "isn’t", "itd": "it’d", "itd’ve": "it’d’ve", "it’dve": "it’d’ve", "itll": "it’ll", "let’s": "let’s", \
+							 "maam": "ma’am", "mightnt": "mightn’t", "mightnt’ve": "mightn’t’ve", "mightn’tve": "mightn’t’ve", "mightve": "might’ve", \
+							 "mustnt": "mustn’t", "mustve": "must’ve", "neednt": "needn’t", "notve": "not’ve", "oclock": "o’clock", "oughtnt": "oughtn’t", \
+							 "ow’s’at": "’ow’s’at", "’ows’at": "’ow’s’at", "’ow’sat": "’ow’s’at", "shant": "shan’t", "shed’ve": "she’d’ve", "she’dve": "she’d’ve", \
+							 "she’s": "she’s", "shouldve": "should’ve", "shouldnt": "shouldn’t", "shouldnt’ve": "shouldn’t’ve", "shouldn’tve": "shouldn’t’ve", \
+							 "somebody’d": "somebodyd", "somebodyd’ve": "somebody’d’ve", "somebody’dve": "somebody’d’ve", "somebodyll": "somebody’ll", \
+							 "somebodys": "somebody’s", "someoned": "someone’d", "someoned’ve": "someone’d’ve", "someone’dve": "someone’d’ve", \
+							 "someonell": "someone’ll", "someones": "someone’s", "somethingd": "something’d", "somethingd’ve": "something’d’ve", \
+							 "something’dve": "something’d’ve", "somethingll": "something’ll", "thats": "that’s", "thered": "there’d", "thered’ve": "there’d’ve", \
+							 "there’dve": "there’d’ve", "therere": "there’re", "theres": "there’s", "theyd": "they’d", "theyd’ve": "they’d’ve", \
+							 "they’dve": "they’d’ve", "theyll": "they’ll", "theyre": "they’re", "theyve": "they’ve", "twas": "’twas", "wasnt": "wasn’t", \
+							 "wed’ve": "we’d’ve", "we’dve": "we’d’ve", "weve": "we've", "werent": "weren’t", "whatll": "what’ll", "whatre": "what’re", \
+							 "whats": "what’s", "whatve": "what’ve", "whens": "when’s", "whered": "where’d", "wheres": "where's", "whereve": "where’ve", \
+							 "whod": "who’d", "whod’ve": "who’d’ve", "who’dve": "who’d’ve", "wholl": "who’ll", "whos": "who’s", "whove": "who've", "whyll": "why’ll", \
+							 "whyre": "why’re", "whys": "why’s", "wont": "won’t", "wouldve": "would’ve", "wouldnt": "wouldn’t", "wouldnt’ve": "wouldn’t’ve", \
+							 "wouldn’tve": "wouldn’t’ve", "yall": "y’all", "yall’ll": "y’all’ll", "y’allll": "y’all’ll", "yall’d’ve": "y’all’d’ve", \
+							 "y’alld’ve": "y’all’d’ve", "y’all’dve": "y’all’d’ve", "youd": "you’d", "youd’ve": "you’d’ve", "you’dve": "you’d’ve", \
+							 "youll": "you’ll", "youre": "you’re", "youve": "you’ve"}
+manualMap    = { 'none': '0',
+							  'zero': '0',
+							  'one': '1',
+							  'two': '2',
+							  'three': '3',
+							  'four': '4',
+							  'five': '5',
+							  'six': '6',
+							  'seven': '7',
+							  'eight': '8',
+							  'nine': '9',
+							  'ten': '10'
+							}
+articles     = ['a','an','the']
+ 
+
+periodStrip  = re.compile("(?!<=\d)(\.)(?!\d)")
+commaStrip   = re.compile("(\d)(\,)(\d)")
+punct        = [';', r"/", '[', ']', '"', '{', '}',
+							 '(', ')', '=', '+', '\\', '_', '-',
+							 '>', '<', '@', '`', ',', '?', '!']
+ 
+
+def processPunctuation(inText):
+    # deal with punctuation
+    outText = inText
+    for p in punct:
+        if (p + ' ' in inText or ' ' + p in inText) or (re.search(commaStrip, inText) != None):
+            outText = outText.replace(p, '')
+        else:
+            outText = outText.replace(p, ' ')	
+    outText = periodStrip.sub("",
+                                    outText,
+                                    re.UNICODE)
+    return outText
+
+# TO-DO could we add wordnetlemmatizer?    
+def wordnetlemmatizer():
+    # rocks->rock; corpora->corpus, etc
+    return True
+def Time():
+    return True
+def spellingcorrection(inText):
+    spell = SpellChecker(distance=1)
+    # find those words that may be misspelled
+    misspelled = spell.unknown(inText)
+    for word in misspelled:
+        corrected=spell.correction(word)
+        if word!=corrected:
+            print(word)
+        # Get the one `most likely` answer
+            print(corrected)
+    return True
+
+def processDigitArticle(inText):
+    # lower case; one->1; delete "a/an/the";  "youll": "you’ll"
+    outText = []
+    tempText = inText.lower().split()
+    for word in tempText:
+        word = manualMap.setdefault(word, word)
+        if word not in articles:
+            outText.append(word)
+        else:
+            pass
+    for wordId, word in enumerate(outText):
+        if word in contractions: 
+            outText[wordId] = contractions[word]
+    outText = ' '.join(outText)
+    return outText
+def processed_answer(anns):
+    new_anns=[]
+    for ann in anns:
+        ann=processPunctuation(ann)
+        ann=processDigitArticle(ann)
+        new_anns.append(ann)
+    return new_anns
+path="train.json"
+cleanpath="train_clean.json"
+clean_ann=[]
+with open(path,encoding="utf-8") as json_file:
+    
+    with open(cleanpath,'w',encoding="utf-8") as json_file_w:
+        datas = json.load(json_file)
+        for data in datas:
+            tmp_ann={}
+            if data["answer_type"]!="unanswerable":
+                tmp_answers=[i['answer'] for i in data["answers"]]
+                unique_answer=list(set(tmp_answers))
+                unique_answer=list(filter(lambda x:x!="unsuitable" and x!="unsuitable image" and x!="unanswerable",unique_answer))
+                p_unique_answer=processed_answer(unique_answer)
+                # if c_p_u_answer!=unique_answer:
+                # print(p_unique_answer)
+                #     print(c_p_u_answer)
+                if len(p_unique_answer)>1:
+                    tmp_ann["answers"]=p_unique_answer
+                    tmp_ann["image"]=data["image"]
+                    tmp_ann["question"]=data["question"]
+                    tmp_ann["answer_type"]=data["answer_type"]
+                    clean_ann.append(tmp_ann)
+        json.dump(clean_ann,json_file_w)
