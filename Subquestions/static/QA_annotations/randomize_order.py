6bcb51a175f8d267eb0a7f141a4114a275659732
陈崇彦 <chongyanchen_hci@utexas.edu>
2020-11-08 23:41:11 -0600

    OneAnsdiffObj

--- Subquestions/static/QA_annotations/randomize_order.py
+++ Subquestions/static/QA_annotations/randomize_order.py
@@ -3,7 +3,7 @@ import json
 with open ('val_clean_v0.json') as f:
     data=json.load(f)
 print(len(data))
-# index = [i for i in range(1000)]
-# random.shuffle(index)
-# print(index)
+index = [i for i in range(1000)]
+random.shuffle(index)
+print(index)
 
