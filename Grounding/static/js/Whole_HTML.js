44490174fcd6c383711e6d145df53afadfd40146
陈崇彦 <chongyanchen_hci@utexas.edu>
2020-11-08 23:41:11 -0600

    OneAnsdiffObj

--- Grounding/static/js/Whole_HTML.js
+++ Grounding/static/js/Whole_HTML.js
@@ -1,50 +1,9 @@
-function ReportProblem() {
-    var x = document.getElementById("ReportProblemDIV");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
 
-function PartialExpertiseChecked() {
-    var Expertise = document.getElementById("EK2");
-    var Partial = document.getElementById("EK3");
-    var task4 = document.getElementById("task4");
-    if (Expertise.checked == true || Partial.checked==true){
-        task4.style.display = "block";
-    } else {
-        task4.style.display = "none";
-    }
-  }
+$("#example-basic").steps({
+    headerTag: "h3",
+    bodyTag: "section",
+    transitionEffect: "slideLeft",
+    autoFocus: true
+});
 
-function DisableOther_P(){
-    
-    if (document.getElementById("Perception").checked==true){
-        document.getElementById("EK1").disabled=true;
-        document.getElementById("EK2").disabled=true;
-        document.getElementById("EK3").disabled=true;
-        document.getElementById("NotSure").disabled=true;}
-    else{
-        
-        document.getElementById("EK1").disabled=false;
-        document.getElementById("EK2").disabled=false;
-        document.getElementById("EK3").disabled=false;
-        document.getElementById("NotSure").disabled=false;
-    }
-}
-function DisableOther_NS(){
-    if (document.getElementById("NotSure").checked==true){
-        document.getElementById("EK1").disabled=true;
-        document.getElementById("EK2").disabled=true;
-        document.getElementById("EK3").disabled=true;
-        document.getElementById("Perception").disabled=true;}
-    else{
-        
-        document.getElementById("EK1").disabled=false;
-        document.getElementById("EK2").disabled=false;
-        document.getElementById("EK3").disabled=false;
-        document.getElementById("Perception").disabled=false;
-    }
 
-}
